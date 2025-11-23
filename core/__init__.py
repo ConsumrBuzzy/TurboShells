0344ce03f47fc66a7dab7e1cf0f865359ff3f6ca
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-11-22 21:48:00 -0500

    ``` fix: Update genetics import to use root-level package instead of core sub-module
    Change genetics import from relative core.genetics to absolute root-level genetics package, remove genetics from core.__all__ exports list
    ```

--- core/__init__.py
+++ core/__init__.py
@@ -3,8 +3,10 @@ Core Module for TurboShells
 Modular architecture following Single Responsibility Principle
 """
 
+# Import genetics from root level
+import genetics
+
 # Import all sub-modules
-from . import genetics
 from . import rendering
 from . import voting
 from . import game
@@ -14,12 +16,12 @@ from . import systems
 from genetics import VisualGenetics
 from rendering import DirectTurtleRenderer, get_direct_renderer
 from voting import VotingSystem, DesignPackage
-from game import Turtle, Race, GameState, KeyboardHandler, RaceTrack, Simulation
+from game import Turtle, GameState, KeyboardHandler, RaceTrack, Simulation
 from systems import StateHandler, GeneticPoolManager, PatternGenerators
 
 __all__ = [
     # Sub-modules
-    'genetics', 'rendering', 'voting', 'game', 'systems',
+    'rendering', 'voting', 'game', 'systems',
     
     # Main classes
     'VisualGenetics', 'DirectTurtleRenderer', 'VotingSystem', 'DesignPackage',
--- core/game/__init__.py
+++ core/game/__init__.py
@@ -3,10 +3,10 @@ Game Logic for TurboShells
 Modular game system following Single Responsibility Principle
 """
 
-from .entities import Turtle, Race
+from .entities import Turtle
 from .game_state import GameState
 from .keyboard_handler import KeyboardHandler
 from .race_track import RaceTrack
 from .simulation import Simulation
 
-__all__ = ['Turtle', 'Race', 'GameState', 'KeyboardHandler', 'RaceTrack', 'Simulation']
+__all__ = ['Turtle', 'GameState', 'KeyboardHandler', 'RaceTrack', 'Simulation']
